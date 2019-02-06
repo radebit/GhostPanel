1c01eb4692dcdafdc2828779485f459f510c2ea7
radebit <26564356@qq.com>
2019-02-06 20:01:26 +0800

    Change Entry

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
+// | Author: Rade
 // +----------------------------------------------------------------------
 
 // [ 应用入口文件 ]
 
 // 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
-//define('APP_PATH', __DIR__ . '/apps/');
+define('APP_PATH', __DIR__ . '/application/');
 // 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+require __DIR__ . '/thinkphp/start.php';
